5ce4f97cae4ce0c61a554c8c9960d493fa43522b
OliverJFedorko <oliver@theo-tech.co.uk>
2022-10-26 12:25:10 +0100

    Added Automated VDA Support

--- PreInstall/IddSampleDriver/installCert.bat
+++ PreInstall/IddSampleDriver/installCert.bat
@@ -1,8 +1,7 @@
-@echo off
+# Install certificates
+certutil -addstore -f root IddSampleDriver.cer
+certutil -addstore -f TrustedPublisher IddSampleDriver.cer
 
-set CERTIFICATE="%~dp0IddSampleDriver.cer"
-
-certutil -addstore -f root %CERTIFICATE%
-certutil -addstore -f TrustedPublisher %CERTIFICATE%
-
-pause
+# Install driver
+nefconw --create-device-node --hardware-id ROOT\iddsampledriver --class-name Display --class-guid 4d36e968-e325-11ce-bfc1-08002be10318
+nefconw --install-driver --inf-path IddSampleDriver.inf
